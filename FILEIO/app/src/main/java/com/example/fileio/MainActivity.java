c506ba2c73777a501632c18c021898e008a5a5be
Sebastian Tellez <sebastian.tellez.o@gmail.com>
2016-11-01 13:50:32 -0600

    ejemplo FILE I/O SD

--- FILEIO/app/src/main/java/com/example/fileio/MainActivity.java
+++ FILEIO/app/src/main/java/com/example/fileio/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.fileio;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,6 +12,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -18,7 +23,11 @@ public class MainActivity extends AppCompatActivity {
 
     EditText editText;
     Button btnGuardar, btnRecuperar;
+    Button btnGuardarSD, btnRecuperarSD;
     TextView txtRecuperado;
+
+    boolean sdDisponible = false;
+    boolean sdAccesoW = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
 
         btnGuardar = (Button) findViewById(R.id.btnGuardar);
         btnRecuperar = (Button) findViewById(R.id.btnRecuperar);
+
+        btnGuardarSD = (Button) findViewById(R.id.btnGuardarSD);
+        btnRecuperarSD = (Button) findViewById(R.id.btnRecuperarSD);
 
         txtRecuperado = (TextView) findViewById(R.id.txtRecuperado);
 
@@ -64,6 +76,59 @@ public class MainActivity extends AppCompatActivity {
                 } catch (IOException e) {
                     e.printStackTrace();
                     Toast.makeText(MainActivity.this, "Error al leer de la memoria", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        final String status = Environment.getExternalStorageState();
+
+        if (status.equals(Environment.MEDIA_MOUNTED)) {
+            sdDisponible = true;
+            sdAccesoW = true;
+        } else if (status.equals(Environment.MEDIA_MOUNTED_READ_ONLY)) {
+            sdDisponible = true;
+            sdAccesoW = false;
+        } else {
+            sdAccesoW = false;
+            sdDisponible = false;
+        }
+
+        btnGuardarSD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (sdDisponible && sdAccesoW) {
+                    try {
+                        File ruta_sd = Environment.getExternalStorageDirectory();
+//                        File ruta_sd2 = Environment.getExternalStorageState(ruta_sd);
+                        File filescritura = new File(ruta_sd.getAbsolutePath(), "andysd.txt");
+                        Log.e("filepath", filescritura.getAbsolutePath() + "");
+                        OutputStreamWriter streamWriterSD =
+                                new OutputStreamWriter(new FileOutputStream(filescritura));
+                        streamWriterSD.write(editText.getText().toString());
+                        streamWriterSD.close();
+                        Toast.makeText(MainActivity.this, "Dato guardado en la SD", Toast.LENGTH_SHORT).show();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        Toast.makeText(MainActivity.this, "Error al escribir en memoria SD", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }
+        });
+
+        btnRecuperarSD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    File ruta_sd = Environment.getExternalStorageDirectory();
+                    File fileRec = new File(ruta_sd.getAbsolutePath(), "andysd.txt");
+                    BufferedReader bufferedReadersd =
+                            new BufferedReader(new InputStreamReader(new FileInputStream(fileRec)));
+                    txtRecuperado.setText(bufferedReadersd.readLine());
+                    bufferedReadersd.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(MainActivity.this, "Error al leer de la SD", Toast.LENGTH_SHORT).show();
                 }
             }
         });
